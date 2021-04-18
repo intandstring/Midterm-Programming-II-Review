11ec8812134e85d907281076f2aef7d5ef8d142a
Alex <alex@intandstring.com>
2021-04-18 15:16:42 +0800

    finished 28,29

---
+++ Question 29/Alex/solution.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+class Set
+{
+  public:
+    Set() {
+        for(int i = 0; i < 100; i++){
+            element[i] = 0;
+        }
+        count = 0;
+    }
+    Set(const Set &s) { //copy 
+        for(int i = 0; i < s.count; i++){
+            element[i] = s.element[i];
+        }
+        count = s.count;
+    }
+
+    int add(const int num){
+        //flags
+        bool numFound = false;
+        for(int i = 0; i < count; i++){
+            if(element[i] == num){
+                numFound = true;
+                break;
+            }
+        }
+        if(!numFound){
+           element[count] = num;
+           count++;
+           return 1; 
+        } else {
+            return 0;
+        }
+    }
+
+    int add(const Set s){
+        bool numFound = false;
+        int added = 0;
+        for(int i = 0; i < s.count; i++){
+            for(int j = 0; j < count; j++){
+                if(s.element[i] == element[j]){
+                    numFound = true;
+                    break;
+                }
+            }
+            if(!numFound){
+                element[count] = s.element[i];
+                count++;
+                added++;
+            }
+            numFound = false;
+        }
+        return added;
+    }
+
+    //Set a,b,c
+    //c = a+b
+    Set operator+ (Set s){
+        Set unioned;
+        for(int i = 0; i < count; i++){
+            unioned.element[i] = element[i];
+        }
+        unioned.count = count;
+        unioned.add(s);
+        return unioned;
+    }
+
+    //c = a - b;
+    Set operator- (Set s){
+        bool numFound = false;
+        Set intersect;
+        for(int i = 0; i < s.count; i++){
+            for(int j = 0; j < count; j++){
+                if(element[j] == s.element[i]){
+                    numFound = true;
+                    break;
+                }
+            }
+            if(numFound){
+                intersect.element[intersect.count] = s.element[i];
+                intersect.count++;
+            }
+            numFound = false;
+        }
+        return intersect;
+    }
+
+  	void display()
+    {
+      int j;
+      for(j = 0; j < count; j ++)
+        cout<<element[j]<<" ";
+    }
+  private:
+  	int element[100];
+  	int count;
+};
